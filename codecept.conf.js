8d4d8f8f74e132c687199b8f6cd24e8d633fba49
Moch. Avin <5025221061@mhs.its.ac.id>
2024-12-01 09:17:18 +0700

    fix: standardize quotation marks and formatting across multiple files

--- codecept.conf.js
+++ codecept.conf.js
@@ -21,4 +21,4 @@ exports.config = {
     I: './steps_file.js'
   },
   name: 'restaurant-apps-starter-project'
-}
+};
--- e2e/Liking_Movies.test.js
+++ e2e/Liking_Movies.test.js
@@ -13,19 +13,19 @@ Scenario('liking and unliking a restaurant', async ({ I }) => {
   // Like restaurant
   I.see('Explore Restaurants', '.restaurant-list-title');
   I.click(locate('.restaurant-card').first());
-  
+
   I.seeElement('#favoriteButton');
   I.click('#favoriteButton');
 
   // Verify restaurant is liked
   I.amOnPage('/#/favorite');
   I.seeElement('.restaurant-card');
-  
+
   // Unlike restaurant
   I.click(locate('.restaurant-card').first());
   I.seeElement('#favoriteButton');
   I.click('#favoriteButton');
-  
+
   // Verify restaurant is removed from favorites
   I.amOnPage('/#/favorite');
   I.see('No Favorite Restaurants', '.empty-favorites');
